09c8b853bdb5dddc98cb86518306ec89600d5a10
foxbloodiron <ariakbar6944@gmail.com>
2018-07-06 10:37:34 +0700

    printout faktur terbilang uang

--- tammafood/resources/views/penjualan/POSgrosir/print_faktur.blade.php
+++ tammafood/resources/views/penjualan/POSgrosir/print_faktur.blade.php
@@ -151,7 +151,28 @@
 			</tr>
 			<tr>
 				<td colspan="5" class="vertical-baseline border-none-right" style="position: relative;">
-					<div class="top s16">Terbilang : Satu Juta Enam Ratus Ribu Rupiah</div>
+					<?php
+					function terbilang($x) {
+					  $angka = ["", "Satu", "Dua", "Tiga", "Empat", "Lima", "Enam", "Tujuh", "Delapan", "Sembilan", "Sepuluh", "Sebelas"];
+					  if ($x < 12)
+					    return " " . $angka[$x];
+					  elseif ($x < 20)
+					    return terbilang($x - 10) . " Belas";
+					  elseif ($x < 100)
+					    return terbilang($x / 10) . " Puluh" . terbilang($x % 10);
+					  elseif ($x < 200)
+					    return "Seratus" . terbilang($x - 100);
+					  elseif ($x < 1000)
+					    return terbilang($x / 100) . " Ratus" . terbilang($x % 100);
+					  elseif ($x < 2000)
+					    return "Seribu" . terbilang($x - 1000);
+					  elseif ($x < 1000000)
+					    return terbilang($x / 1000) . " Ribu" . terbilang($x % 1000);
+					  elseif ($x < 1000000000)
+					    return terbilang($x / 1000000) . " Juta" . terbilang($x % 1000000);
+					}
+					?>
+					<div class="top s16">Terbilang : <?php echo ucwords(terbilang($dataTotal[0]->total)); ?> Rupiah</div>
 					<div class="float-left" style="width: 40vw;">
 						<ul style="padding-left: -15px;">
 							<li>Barang yang sudah dibeli tidak bisa dikembalikan lagi kecuali ada perjanjian</li>
